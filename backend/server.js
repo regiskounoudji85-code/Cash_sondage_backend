1a2cc0a7a08fb429aebd389e8c251071025a0621
regiskounoudji85-code <regiskounoudji85@gmail.com>
2026-07-29 11:51:13 +0000

    fix trust proxy

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const withdrawalRoutes = require('./routes/withdrawal');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
